2572f9db77d351471e6cdf13639248b45e20aa37
sreedhar111yu <sreedharv36@gmail.com>
2024-02-29 18:19:33 +0530

    inventory icon & bar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Home from './pages/Home'
 
 function App() {
   return (
    <>
-   App
+   <BrowserRouter>
+     <Sidebar/>
+     
+     <Routes>
+      <Route path='/' element={<Home/>}></Route>
+     </Routes>
+   </BrowserRouter>
    </>
   )
 }
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      'madimi-one': ['Madimi One', 'sans-serif'],
+    },
+  },
   },
   plugins: [],
 }
