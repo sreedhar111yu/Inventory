5e7b2397bf62e6a52928c0c20de053c808e641c7
sreedhar111yu <sreedharv36@gmail.com>
2024-02-29 21:49:46 +0530

    navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
+import  Navbar  from './components/Navbar'
 
 function App() {
   return (
    <>
    <BrowserRouter>
      <Sidebar/>
-     
+     <Navbar/>
+
      <Routes>
       <Route path='/' element={<Home/>}></Route>
      </Routes>
--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -17,8 +17,8 @@ function Sidebar() {
   };
   return (
     <>
-     <div className='w-full h- flex text-white '>
-    <div className='h-[calc(102vh-1rem)] w-full max-w-[15rem] bg-black p-4 shadow-xl shadow-blue-gray-900/5 '>
+     <div className='w-full h- flex '>
+    <div className='h-[calc(102vh-1rem)] w-full max-w-[15rem] bg-black p-4 shadow-xl shadow-blue-gray-900/5 text-white '>
     <Link to='/' className='flex space-x-2'>  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
 </svg>
@@ -71,21 +71,21 @@ Inventory</h1></Link>
               className="block px-4 py-2 text-sm hover:text-gray-900"
               role="menuitem"
             >
-              Option 1
+              Items
             </a>
             <a
               href="#"
               className="block px-4 py-2 text-sm  hover:text-blue-900"
               role="menuitem"
             >
-              Option 2
+              Price List
             </a>
             <a
               href="#"
               className="block px-4 py-2 text-sm  hover:text-blue-900"
               role="menuitem"
             >
-              Option 3
+              Item Groups
             </a>
           </div>
         </div>
@@ -131,21 +131,21 @@ Inventory</h1></Link>
               className="block px-4 py-2 text-sm hover:text-gray-900"
               role="menuitem"
             >
-              Option 1
+              Customer
             </a>
             <a
               href="#"
               className="block px-4 py-2 text-sm  hover:text-blue-900"
               role="menuitem"
             >
-              Option 2
+              Sales Orders
             </a>
             <a
               href="#"
               className="block px-4 py-2 text-sm  hover:text-blue-900"
               role="menuitem"
             >
-              Option 3
+              Shipments
             </a>
           </div>
         </div>
@@ -191,22 +191,22 @@ Inventory</h1></Link>
               href="#"
               className="block px-4 py-2 text-sm hover:text-gray-900"
               role="menuitem"
-            >
-              Option 1
+            >vendors
+              
             </a>
             <a
               href="#"
               className="block px-4 py-2 text-sm  hover:text-blue-900"
               role="menuitem"
             >
-              Option 2
+              Purchase Orders
             </a>
             <a
               href="#"
               className="block px-4 py-2 text-sm  hover:text-blue-900"
               role="menuitem"
             >
-              Option 3
+              Bills
             </a>
           </div>
         </div>
@@ -232,6 +232,46 @@ Inventory</h1></Link>
 
     
     </div> 
+
+    {/* Navbar */}
+
+    <div className='w-full    '>
+        <div className='  max-w-[1280px] bg-slate-200  w-full p-4 border flex  justify-between '>
+        <div className='flex border w-1/4 bg-white rounded-lg items-center'>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-8 font-bold">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+  </svg>
+
+  <h1 className='text-lg px-5'>
+    <input type="text" placeholder='Search' className='focus:outline-none' />
+  </h1>
+</div>
+     
+
+     
+<div className=' flex space-x-5 mt-1 '>
+    <Link>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-7">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
+</svg>
+    </Link>
+     
+   <Link> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-7">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg></Link>
+
+
+
+
+     </div>
+        
+       
+        </div>
+       
+        
+    </div>
+    
+
     </div>
    </>
   )
