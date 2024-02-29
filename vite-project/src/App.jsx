f8edf262c7e08a457e04556ccbfbaa56979b85b3
sreedhar111yu <sreedharv36@gmail.com>
2024-02-29 21:50:58 +0530

    del

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
-import  Navbar  from './components/Navbar'
+
 
 function App() {
   return (
    <>
    <BrowserRouter>
      <Sidebar/>
-     <Navbar/>
+     
 
      <Routes>
       <Route path='/' element={<Home/>}></Route>
